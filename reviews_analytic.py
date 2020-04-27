490d3c86f1c38dbca68ba9adcabb5c72617440e9
Sunny <61953314+githubsliu2020@users.noreply.github.com>
2020-04-26 17:19:37 -0700

    update reviews_alaytic.py

--- reviews_analytic.py
+++ reviews_analytic.py
@@ -1,15 +1,44 @@
-data= []   #建議data清單
+data= []   #建立data清單
 count = 0
 with open('reviews.txt','r') as f: #打開reviews.txt 並命名為f:
 	for line in f: #將f:檔案裡面的每一行
 		data.append(line) #加入data清單中
 		count += 1 #count=count+1
-		if count % 10000 == 0 : #只要count 除以10000餘數是0
-				print(len(data)) #列印出data清單的每一筆(會一筆一筆印)
+		if count % 1000 == 0: #只要count 除以1000餘數是0
+			print(len(data)) #列印出data清單的每一筆(會一筆一筆印)
 print(len(data)) #印出data的總資料長度
-print(data[10])#印出data清單中的索引為10的資料
+#print(data[10])#印出data清單中的索引為10的資料
 
 print ('檔案讀取完畢,總共有',len(data),'筆資料') # 
+
+print(data[0])
+
+#文字計數S
+wc = {}
+for d in data:    #d 是單留言 data是整筆留言清單
+	words = d.split(' ')  #把每個字串用空白分開
+	for word in words:    #把每一個words留言內的每個字
+		if word in wc:    #如果這個字word在字典裡有出現過   
+			wc[word] += 1  #查找字典中word這個字,如果有出現就加1
+		else:
+ 			wc[word] = 1     #如果沒有出現過,就新增key進wc字典,次數為1
+for word in wc:
+	if wc[word] > 100000:   #如果重複的字詞出現超過100000次
+		print(word, wc[word])
+print(len(wc))
+# #print(wc['Allen'])
+
+while True:
+	word = input(' 請問你想查什麼字:')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為',wc[word])
+	else:
+		print('這個字沒有出現過')
+
+print('感謝使用本查詢功能')
+
 
 sum_len = 0
 for d in data:  #將data清單中每一筆留言d
